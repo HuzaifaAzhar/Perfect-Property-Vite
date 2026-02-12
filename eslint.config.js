55d458ddd39e80df3a8418111129252a762150e3
Huzaifa Azhar <huzaifaazhar21@gmail.com>
2026-02-12 17:57:12 +0500

    Auto-remove injected malicious payload

--- eslint.config.js
+++ eslint.config.js
@@ -23,5 +23,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-]);
-
+]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
